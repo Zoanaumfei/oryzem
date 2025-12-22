483c112af1ed74a98dd9d805104717efebae0b68
Zoanaumfei <65295596+Zoanaumfei@users.noreply.github.com>
2025-12-22 15:49:37 -0300

    login

--- src/vanderson/js/cognito-config.js
+++ src/vanderson/js/cognito-config.js
@@ -2,7 +2,7 @@
 
 const COGNITO_CONFIG = {
   UserPoolId: "us-east-1_tuCP4JyT5",
-  ClientId: "37366mm0u7r51henlt049759if",
+  ClientId: "38o16atrmejvgth708cifvbti",
 };
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(COGNITO_CONFIG);
