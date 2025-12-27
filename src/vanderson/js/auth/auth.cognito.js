c3a73050d3ba414ed3049873bd2065a6ffbfc791
Zoanaumfei <65295596+Zoanaumfei@users.noreply.github.com>
2025-12-26 22:44:57 -0300

    userpool

--- src/vanderson/js/auth/auth.cognito.js
+++ src/vanderson/js/auth/auth.cognito.js
@@ -1,9 +1,7 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
 const poolData = {
   UserPoolId: "us-east-1_tuCP4JyT5",
   ClientId: "38o16atrmejvgth708cifvbti"
 };
 
-export const userPool = new CognitoUserPool(poolData);
+export const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
